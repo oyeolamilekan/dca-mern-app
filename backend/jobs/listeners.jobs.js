79102f43c16a4b5a1ee1c1ea1c7f6a9f001eac4d
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-08-04 09:11:36 +0100

    chore: configured redis url for prod.

--- backend/jobs/listeners.jobs.js
+++ backend/jobs/listeners.jobs.js
@@ -1,11 +1,13 @@
 const Bull = require("bull")
 const transactionModel = require("../models/transaction.model")
 
+const REDIS_URL = process.env.REDIS_URL
+
 /**
  * Process the data that has been added to the 'mail-queue'
  */
 const sendEmailQueueListener = async () => {
-    const emailProcessingQueue = new Bull("mail-queue")
+    const emailProcessingQueue = new Bull("mail-queue", REDIS_URL)
     emailProcessingQueue.process(async (jobs) => {
         await sendMail(jobs.data)
     })
@@ -19,7 +21,7 @@ const sendMail = async (data) => {
  * Process the instant order that has been added to the instant-order-final-process queue.
  */
 const updateInstantOrderFromWebhook = async () => {
-    const transactionProcessingQueue = new Bull("instant-order-final-process")
+    const transactionProcessingQueue = new Bull("instant-order-final-process", REDIS_URL)
     transactionProcessingQueue.process(async (jobs) => {
         await processTransaction(jobs.data)
     })
