c38b54598263615a736fbecf8c54a09ed59bb2b7
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-08-05 14:07:53 +0100

    chore: added middleware request response logs,

--- backend/middleware/request.middleware.js
+++ backend/middleware/request.middleware.js
@@ -1,8 +1,15 @@
 const logger = require("../services/logging.service");
 
-const requestMiddleware = (err,req, res, next) => {
-    logger.info(JSON.stringify({ "path": req.path, "method": req.method }))
-    next()
+const requestMiddleware = (req, res, next) => {
+
+    logger.info(`REQUEST - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        /** Log the res */
+        logger.info(`RESPONSE - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+    });
+
+    next();
 }
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,8 @@ const app = express()
 
 app.use(cors());
 
+app.use(requestMiddleware)
+
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
@@ -37,8 +39,6 @@ app.use("/api/plan", require("./routes/plan.route"))
 app.use("/api/transaction", require("./routes/transaction.route"))
 
 app.use(errorHandler)
-
-app.use(requestMiddleware)
 
 app.use('*', function (req, res) {
     res.status(404).json({ data: 'route not found' });
