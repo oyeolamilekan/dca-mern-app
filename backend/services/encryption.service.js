f3cd1ffac75578f7e43e27df9c4fc5fdd743a337
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-08-08 00:18:31 +0100

    chore: cleans on utils service.

--- backend/services/encryption.service.js
+++ backend/services/encryption.service.js
@@ -8,9 +8,7 @@ const encrypt = (text) => {
     let iv = crypto.randomBytes(IV_LENGTH);
     let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
     let encrypted = cipher.update(text);
-
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-
     return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
