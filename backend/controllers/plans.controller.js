2cbc28fca72a582f72d52e154d56eba1f51c0e9a
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-08-10 21:17:34 +0100

    feat: removed unused variables.

--- backend/controllers/plans.controller.js
+++ backend/controllers/plans.controller.js
@@ -102,10 +102,6 @@ const fetchPlans = async (req, res) => {
     try {
         const page = Number(req.query.page) || 1;
 
-        const limit = Number(req.query.limit) || 10;
-
-        const skip = (page - 1) * limit;
-
         const plans = await Plans.find({ user: req.user.id }).paginate({ page: page })
 
         return res.status(200).send({ hits: plans.length, plans })
